8fafe911e1754b3cec1585be893271de841ebbea
tomof <fffjt007@gmail.com>
2018-10-21 00:18:51 +0900

    Set up webpack

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import Vue from 'vue'
+import TheHeader from './components/TheHeader.vue'
+window.TheHeader = Vue.component('TheHeader', TheHeader)
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,55 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
+const webpack = require('webpack')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const WriteFilePlugin = require('write-file-webpack-plugin')
 
 module.exports = {
+  mode: 'production',
   entry: {
     index: './src/index.js',
-    about: './src/about.js',
+    tree: './src/tree.js',
   },
   output: {
     filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist', '_screen-spec-md'),
   },
   resolve: {
-    extensions: ['.js', '.json', '.jsx'],
+    extensions: ['.js'],
   },
   module: {
     rules: [
       {
-        test: /\.txt$/,
-        use: 'raw-loader',
+        test: /\.vue$/,
+        loader: 'vue-loader',
       },
       {
-        test: /(\.js$|\.jsx$)/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        test: /\.js$/,
+        loader: 'babel-loader',
       },
       {
         test: /\.scss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localIdentName: '[path][name]__[local]--[hash:base64:5]',
-            },
-          },
-          {
-            loader: 'sass-loader',
-          },
-        ],
+        use: ['vue-style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
-  ],
   optimization: {
     splitChunks: {
       chunks: 'all',
     },
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    hot: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new CopyWebpackPlugin([{ from: 'public/**/*', to: '../', flatten: true }]),
+    new CopyWebpackPlugin([{ from: 'node_modules/vue/dist/vue.min.js', to: './' }]),
+    new WriteFilePlugin(), // This is necessary for CopyWebpackPlugin used with webpack-dev-server
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+  ],
 }
