01177147ba4ef682704628046def75a8f4567b60
tomof <fffjt007@gmail.com>
2019-03-16 13:57:36 +0900

    add GET markdown API to editable sever

--- server/editable.js
+++ server/editable.js
@@ -1,15 +1,24 @@
 const path = require('path')
+const fs = require('fs')
 const express = require('express')
 const multer = require('multer')
 
-const editable = express()
-editable.use(express.static('_spec')) //TODO: 指定可能に
+const app = express()
+app.use(express.static('_spec')) //TODO: _specを外部から指定可能に
 
-const upload = multer({ dest: path.join(__dirname, '__uploads') })
-editable.post('/profile', upload.single('avatar'), function(req, res) {
+const uploads = multer({ dest: path.join(__dirname, '__uploads/') })
+app.post('/__image', uploads.single('image'), function(req, res) {
   console.log('uploaded filename： ' + req.file.originalname)
   console.log('saved path：' + req.file.path)
   console.log('saved filename： ' + req.file.filename)
 })
 
-editable.listen(3000, () => console.log('screen-spec-md: Writer mode: listening on port 3000!'))
+app.get('/__markdown', (req, res) => {
+  console.log('req.query', req.query)
+  console.log('process.cwd()', process.cwd())
+  const mdPath = path.resolve(process.cwd(), 'example', `.${req.query.path}.md`) //TODO: exampleを外部から指定可能に
+  const mdContent = fs.readFileSync(mdPath, { encoding: 'utf-8' })
+  res.send(mdContent)
+})
+
+app.listen(3000, () => console.log('screen-spec-md: Writer mode: listening on port 3000!'))
