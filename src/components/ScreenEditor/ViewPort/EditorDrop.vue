442839cd0bd719cf8d72283be1daa629a68b5e9d
tomof <fffjt007@gmail.com>
2019-08-29 17:07:30 +0900

    implement the feature that allow you to paste a screen image from the clipboard

--- src/components/ScreenEditor/ViewPort/EditorDrop.vue
+++ src/components/ScreenEditor/ViewPort/EditorDrop.vue
@@ -1,10 +1,13 @@
 <template>
   <div
+    contenteditable="true"
     class="EditorDrop"
+    @keydown="onKeyDown"
     @dragenter.prevent="onDragEnter"
     @dragover.prevent="onDragOver"
     @dragleave.prevent="onDragLeave"
     @drop.prevent="onDrop"
+    @paste.prevent="onPaste"
   >
     <div class="EditorDrop_Target">
       <svg class="EditorDrop_Icon" width="50" height="43" viewBox="0 0 50 43">
@@ -12,16 +15,26 @@
           d="M48.4 26.5c-.9 0-1.7.7-1.7 1.7v11.6h-43.3v-11.6c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v13.2c0 .9.7 1.7 1.7 1.7h46.7c.9 0 1.7-.7 1.7-1.7v-13.2c0-1-.7-1.7-1.7-1.7zm-24.5 6.1c.3.3.8.5 1.2.5.4 0 .9-.2 1.2-.5l10-11.6c.7-.7.7-1.7 0-2.4s-1.7-.7-2.4 0l-7.1 8.3v-25.3c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v25.3l-7.1-8.3c-.7-.7-1.7-.7-2.4 0s-.7 1.7 0 2.4l10 11.6z"
         />
       </svg>
-      Drag an image here.
+      Drag an image or paste clipboard image here.
     </div>
   </div>
 </template>
 
 <script>
 import loadImage from '../../../modules/loadImage'
+import singleDTHandler from '../../../modules/singleDataTransferHandler'
 export default {
   name: 'EditorDrop',
   methods: {
+    onKeyDown(e) {
+      // allow only paste
+      if (!e.ctrlKey && !e.metaKey) {
+        e.preventDefault()
+        e.stopPropagation()
+        return false
+      }
+      return e.keyCode === 86
+    },
     onDragEnter(e) {},
     onDragOver(e) {},
     onDragLeave(e) {},
@@ -58,6 +71,26 @@ export default {
         reader.readAsDataURL(file)
       })
     },
+    async onPaste(e) {
+      const clipboardData = e.clipboardData
+      if (!singleDTHandler.isSingleImageFile(clipboardData)) return true
+      const imagePath = prompt('Please enter the image file path.', './img/undefined.png')
+      if (imagePath === null) return false
+      const imageFile = singleDTHandler.getAsSingleFile(clipboardData)
+      const imageBase64 = await singleDTHandler.readBase64(clipboardData)
+      const { width, height } = await loadImage(imageBase64)
+      this.setImage(
+        {
+          src: imageBase64,
+          filename: imagePath,
+          width,
+          height,
+        },
+        {
+          fileToUpload: imageFile,
+        },
+      )
+    },
     setImage({ src, filename, width, height }, { fileToUpload }) {
       this.$emit('setImage', { src, filename, width, height }, { fileToUpload })
     },
@@ -67,6 +100,7 @@ export default {
 
 <style lang="scss" scoped>
 .EditorDrop {
+  cursor: default;
   color: #0f3c4b;
   font-size: 18px;
   position: absolute;
@@ -82,6 +116,8 @@ export default {
   background: #c8dadf;
   transition: all 0.1s;
   &_Target {
+    cursor: default;
+    user-select: none;
     pointer-events: none;
     vertical-align: middle;
     display: table-cell;
